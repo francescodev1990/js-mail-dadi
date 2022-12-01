93b944d84b59dfd5d471b7935f5b787cb616b555
Your Name <almannofrancesco@gmail.com>
2022-12-01 16:39:39 +0100

    list of email insert prompt request insert

---
+++ js email/js/script.js
@@ -0,0 +1,9 @@
+
+//LISTA EMAIL PRESENTI IN ARCHIVIO//
+
+const mail = ['almannofrancesco@gmail.com', 'francescoalmanno@gmail.com', 'almannofrancesco@hotmail.it', 'fabriziosgargiulo@live.it', 'snackaperitivo@libero.it'];
+
+//INPUT RICHIESTA PER INSERIMENTO EMAIL UTENTE//
+
+let user_mail = prompt('Per favore inserisci la tua email');
+console.log(user_mail);
