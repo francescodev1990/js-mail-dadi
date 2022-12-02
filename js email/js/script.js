7449e194ba935764e83c65e092864269f195f712
Your Name <almannofrancesco@gmail.com>
2022-12-02 09:55:21 +0100

    exercise email completed js

--- js email/js/script.js
+++ js email/js/script.js
@@ -8,6 +8,20 @@ const mail = ['almannofrancesco@gmail.com', 'francescoalmanno@gmail.com', 'alman
 let user_mail = prompt('Per favore inserisci la tua email');
 console.log(user_mail);
 
-mail.length
-console.log(mail);
+let control_mail = false;
 
+for(let i = 0; i < mail.length; i++){
+    if(user_mail == mail[i]){
+        control_mail = true;
+    }
+    else{
+        control_mail = false;
+    }
+}
+
+if(mail){
+    console.log('Email trovata in archivio');
+}
+else{
+    console.log('Email non trovata in archivio');
+}
