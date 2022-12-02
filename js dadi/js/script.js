332fae5e95b4aadd9b74fbe7f7842af4c31e3c20
Your Name <almannofrancesco@gmail.com>
2022-12-02 10:10:38 +0100

    exercise dadi in progress generate random number

---
+++ js dadi/js/script.js
@@ -0,0 +1,11 @@
+alert('Premi per lanciare i dadi!');
+
+let human_player;
+let pc_player;
+let dadi;
+
+for(i = 0; i < 1000; i++) {
+    dadi = Math.floor(Math.random()* 6 + 1);
+}
+console.log(dadi)
+
