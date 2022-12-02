ab8ee6793bc67c55599b89e024c2e65d6d61b684
Your Name <almannofrancesco@gmail.com>
2022-12-02 10:28:37 +0100

    lancio dadi con vincitore inseriti

--- js dadi/js/script.js
+++ js dadi/js/script.js
@@ -1,11 +1,23 @@
 alert('Premi per lanciare i dadi!');
 
-let human_player;
-let pc_player;
+
 let dadi;
 
 for(i = 0; i < 1000; i++) {
     dadi = Math.floor(Math.random()* 6 + 1);
 }
 console.log(dadi)
+
+let human_player = Math.floor(Math.random()* 6 + 1);
+console.log(human_player);
+let pc_player = Math.floor(Math.random()* 6 + 1);
+console.log(pc_player);
+
+if (human_player > pc_player){
+    console.log('Human_player ha vinto');
+}
+else{
+    console.log('Human_player Ha perso');
+}
+
 
